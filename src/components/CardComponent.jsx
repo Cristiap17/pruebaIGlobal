ebb728212f1c1fb195f11055c56e0cdd64464984
Cristian Pineda <cristiap17@gmail.com>
2023-08-02 13:53:27 -0500

    first commit

---
+++ src/components/CardComponent.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './CardComponent.css'
+import mobileHeader from '../assets/image-header-mobile.jpg'
+
+export default function CardComponent() {
+    return (
+        <div className='card-container'>
+            <div className='header-container'>
+                <img src={mobileHeader} alt="Mobile Header" />
+                <div></div>
+            </div>
+            <div></div>
+            <div className='main-container'>
+                <h1>Get <span className='highlight'>insights</span> that help your business grow.</h1>
+                <p>Discover the benefits of data analitycs and make better decisions reganding revenue, customer experience, and overall efficency.
+                </p>
+            </div>
+            <div className='footer-container'>
+                <div>
+                    <h2>10K+</h2>
+                    <span>COMPANIES</span>
+                </div>
+                <div>
+                    <h2>314</h2>
+                    <span>TEMPLATES</span>
+                </div>
+                <div>
+                    <h2>12M+</h2>
+                    <span>QUERIES</span>
+                </div>
+            </div>
+        </div>
+    )
+}
